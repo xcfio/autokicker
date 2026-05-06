2757c5d0ab871018f3d1d25c32e2f09679704b2d
xcfio <119097812+xcfio@users.noreply.github.com>
2026-05-06 07:21:22 +0600

    refactor: remove database connection setup and update bot package scripts
    - Deleted database connection setup in `apps/backend/src/database/index.ts`.
    - Updated `apps/bot/package.json` scripts to include new build and database commands, and modified development and formatting scripts.
    - Refactored environment variable handling in `apps/bot/src/config.ts` to ensure required variables are present.
    - Added new icon files (`icon.png` and `icon.svg`) to `apps/frontend/public`.
    - Created new drizzle configuration file `lib/databasse/drizzle.config.ts`.
    - Added `package.json` and `tsconfig.json` for the new database library in `lib/databasse`.
    - Updated `pnpm-lock.yaml` to include dependencies for the new database library.
    - Updated Turbo repo schema version in `turbo.json`.

--- apps/bot/src/config.ts
+++ apps/bot/src/config.ts
@@ -1,2 +1,10 @@
-import config from "../config.json"
-export default config
+function env(parm: string): string | never {
+    const param = process.env[parm.toUpperCase()]
+    if (!param) throw new Error(`Environment variable not found: ${parm}`)
+    return param
+}
+
+export default {
+    token: env("token"),
+    uri: env("uri")
+}
