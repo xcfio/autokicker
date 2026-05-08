85c473bf4c6ff4e83a5ffd828cb953a15e70472e
xcfio <119097812+xcfio@users.noreply.github.com>
2026-05-08 09:48:46 +0600

    chore: refactor error handling and update message structure in cx function

--- apps/bot/src/function/error.ts
+++ apps/bot/src/function/error.ts
@@ -1,4 +1,11 @@
-import { DiscordAPIError, MessageFlags, RepliableInteraction } from "discord.js"
+import {
+    ComponentType,
+    DiscordAPIError,
+    InteractionReplyOptions,
+    MessageCreateOptions,
+    MessageFlags,
+    RepliableInteraction
+} from "discord.js"
 import { isSendable } from "./logic"
 
 const ignore = new Set([10008, 10062, 50027])
@@ -8,22 +15,35 @@ export function ex(error: any) {
 }
 
 export async function cx(interaction: RepliableInteraction, error?: Error) {
-    const msg: any = {
-        content: "500 - An unknown error happened. Run `/bug` to report this issue",
-        flags: MessageFlags.Ephemeral
-    }
     if (interaction.replied) return
+    const msg: InteractionReplyOptions & MessageCreateOptions = {
+        flags: [MessageFlags.IsComponentsV2],
+        components: [
+            {
+                type: ComponentType.Container,
+                components: [
+                    {
+                        type: ComponentType.TextDisplay,
+                        content: "# An unknown error occurred."
+                    },
+                    {
+                        type: ComponentType.TextDisplay,
+                        content: "An unknown error happened. Run `/bug` to report this issue"
+                    }
+                ]
+            }
+        ]
+    }
 
     try {
         if (error instanceof DiscordAPIError && ignore.has(Number(error.code))) {
-            msg.flags = undefined
             if (isSendable(interaction.channel)) return await interaction.channel.send(msg)
         } else {
+            msg.flags = [...(msg.flags as Array<InteractionReplyOptions["flags"]>), MessageFlags.Ephemeral] as any
             return await (interaction.deferred ? interaction.followUp(msg) : interaction.reply(msg))
         }
     } catch (error) {
         ex(error)
-        msg.flags = undefined
         if (isSendable(interaction.channel)) return await interaction.channel.send(msg)
     }
 }
