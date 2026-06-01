041e8dde6c94d334ff8989aad6286b9e6e682cf2
xcfio <119097812+xcfio@users.noreply.github.com>
2026-06-01 11:43:31 +0600

    feat: add dynamic preset support

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite"
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2"
 import { solidStart } from "@solidjs/start/config"
+import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig({
-    plugins: [solidStart(), tailwindcss(), nitro()]
-})
+const preset = process.env.SERVER_PRESET ?? process.env.NITRO_PRESET ?? "node-server"
+export default defineConfig({ plugins: [solidStart(), tailwindcss(), nitro({ preset })] })
