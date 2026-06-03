225f35cbf42373742e893abea8c32868b49e57c6
xcfio <119097812+xcfio@users.noreply.github.com>
2026-06-03 18:12:11 +0600

    fix: fix image

---
+++ apps/bot/src/utils/emoji.ts
@@ -0,0 +1,7 @@
+export const Emoji = {
+    /**
+     * @preview ![img](https://cdn.discordapp.com/emojis/1511703911742832660.png) - https://cdn.discordapp.com/emojis/1511703911742832660.png
+     * @id 1511703911742832660
+     */
+    autokicker: "1511703911742832660"
+}
