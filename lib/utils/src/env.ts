4a8713201322c2cda07002b42b306032b2345e5d
xcfio <119097812+xcfio@users.noreply.github.com>
2026-06-30 19:02:07 +0600

    refactor: improve naming of env function

--- lib/utils/src/env.ts
+++ lib/utils/src/env.ts
@@ -1,13 +1,13 @@
 export function env<T extends Record<string, string>, K extends keyof T = keyof T>(
     Key: K,
-    Throw: false
+    error: false
 ): T[K] | undefined
-export function env<T extends Record<string, string>, K extends keyof T = keyof T>(Key: K, Throw?: true): T[K]
+export function env<T extends Record<string, string>, K extends keyof T = keyof T>(Key: K, error?: true): T[K]
 export function env<T extends Record<string, string>, K extends keyof T = keyof T>(
     Key: K,
-    Throw: boolean = true
+    error: boolean = true
 ): T[K] {
     const param = typeof Key === "string" ? process.env[Key.toUpperCase()] : process.env[Key as any]
-    if (!param && Throw) throw new Error(`Environment variable not found: ${String(Key)}`)
-    return (!param && !Throw ? param : String(param)) as T[K]
+    if (!param && error) throw new Error(`Environment variable not found: ${String(Key)}`)
+    return (!param && !error ? param : String(param)) as T[K]
 }
