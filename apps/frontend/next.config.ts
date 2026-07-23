c2a8812a4687cf4e06615a08809ef5bf2ba66f88
xcfio <119097812+xcfio@users.noreply.github.com>
2026-07-23 12:57:36 +0600

    chore: update minimum release age exclusions for various Radix UI and Turbo packages

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {/* config options here */}
+const nextConfig: NextConfig = {
+    experimental: {
+        // @ts-expect-error: This is a valid option, but the type definitions are not up to date yet.
+        useTypeScriptCli: true
+    }
+}
 
 export default nextConfig
