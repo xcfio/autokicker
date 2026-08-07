cc418abee62a81e6015323d289fb80605b43acf0
xcfio <119097812+xcfio@users.noreply.github.com>
2026-08-07 20:13:06 +0600

    build(deps): update typebox to ^1.3.11 and bump rolldown to ^1.2.3

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -383,12 +383,13 @@ export default () => {
                             >
                                 Add Autokicker for Free
                             </ShinyButton>
-                            <Link
+                            {/* TODO: Need to implement this */}
+                            {/* <Link
                                 href="/dashboard"
                                 className="flex items-center justify-center w-full sm:w-auto text-lg gap-2 px-8 py-4 rounded-full text-blue-300 border border-blue-500/20 hover:border-blue-500/40 hover:text-white  bg-blue-950/20 hover:bg-blue-500/5 transition-all duration-200 font-medium"
                             >
                                 Dashboard
-                            </Link>
+                            </Link> */}
                         </div>
 
                         {/* stats */}
